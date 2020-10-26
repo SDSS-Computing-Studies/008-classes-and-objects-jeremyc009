f555ce181a6c5e058fcd273468ad9a1a96ee490d
jeremyc009 <jeremyc009@deltalearns.ca>
2020-10-26 12:55:46 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -25,15 +25,71 @@ constructor     - should require the student name, studentNumber and grade (in t
 class student:
 
     # properties should be listed first
+    name = ''
+    studentNumber = ''
+    grade=0
+    courses=[]
+    grades=[]
 
-    def __init__(): # You will need to create your own input parameters for all methods
-        pass
+    def __init__(self,name,studentNumber,grade,courses=[],grades=[]): # You will need to create your own input parameters for all methods
+        self.name = name
+        self.studentNumber = studentNumber
+        self.grade = grade
+        self.courses = courses
+        self.grades = grades
 
-    def __del__():
-        pass
+    def __del__(self):
+        print("Thank you for viewing the profile of " +self.name +'.')
 
-    def average(self):
-        pass
+    def average(self,grades):
+        length=len(grades)
+        num2=0
+        for i in range(0,length):
+            num1=grades[i]
+            num2=num2+num1
+        return num2
+    def getHonorRoll(self):
+        lis=self.grades
+        length=len(lis)
+        if length<5:
+            ans=print('Student does not have enough courses.')
+            return ans
+        else: 
+            lis.sort()
+            average=(lis[-1]+lis[-2]+lis[-3]+lis[-4]+lis[-5])/5
+            return average
+    def showCourses(self):
+        lis=self.courses
+        ans=print(lis)
+        return ans
+    def showGrade(self,ind):
+        class1=self.courses
+        course=class1[ind]
+        grades=self.grades
+        grades2=grades[ind]
+        ans=print(self.name+' has a '+ str(grades2) + '%'+' in the course' + str(course)+'.')
+        return ans
+    def getCourses(self,lis1):
+        
+        self.courses=lis1
+        
+    def getGrades(self,grade1,grade2,grade3,grade4,grade5,grade6,grade7):
+        lis1=[]
+        lis1.insert(grade1,0)
+        lis1.insert(grade2,1)
+        lis1.insert(grade3,2)
+        lis1.insert(grade4,3)
+        lis1.insert(grade5,4)
+        lis1.insert(grade6,5)
+        lis1.insert(grade7,6)
+        
+        self.grades=lis1
+        
+        
+        
+
+
+
 
 def main():
     # This contains test data that will be used by the autograder.
